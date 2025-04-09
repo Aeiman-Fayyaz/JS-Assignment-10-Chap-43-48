75ef495bd6debc69d01ac45c8311d7d522ccf577
Aeiman-Fayyaz <aeimanfayyaz2002@gmail.com>
2025-04-10 02:07:42 +0500

    code update

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 // QUESTION 1
 // Show an alert box on click on a link.
+
 // ANSWER 1
+
+function greet() {
+  alert("Thanks for order");
+}
+
 // QUESTION 2
+// Display some Mobile images in browser. On click on an
+//     image Show the message in alert to user.
+
 // ANSWER 2
+
+function thanksForPurchasing() {
+    alert("Order confirm \nThanks for Purchasing")
+    
+}
+
 // QUESTION 3
+// Display 10 student records in table and each row should contain a delete
+// button. If you click on a button to delete a record, entire row should be
+// deleted. 
+
 // ANSWER 3
+
+
 // QUESTION 4
+// Display an image in browser. Change the picture on mouseover and set the
+// first picture on mouseout.
+
 // ANSWER 4
+
 // QUESTION 5
+
 // ANSWER 5
